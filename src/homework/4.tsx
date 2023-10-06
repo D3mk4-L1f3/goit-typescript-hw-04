79ec62271985da36f6c570d26acf9f647bc2c964
D3mk4 <dmtvchenko@gmail.com>
2023-10-06 11:27:24 +0300

    update 1.2

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -3,11 +3,17 @@ import noop from "lodash/noop";
 import { first } from "lodash";
 
 type MenuIds = "first" | "second" | "last";
+
 type Menu = { id: MenuIds; title: string };
+
 type SelectedMenu = { id: MenuIds };
+
 type MenuSelected = { selectedMenu: SelectedMenu };
+
 type MenuAction = { onSelectedMenu: (selectedMenu: SelectedMenu) => void };
+
 type PropsProvider = { children: ReactNode };
+
 type PropsMenu = { menus: Menu[] };
 
 const MenuSelectedContext = createContext<MenuSelected>({
